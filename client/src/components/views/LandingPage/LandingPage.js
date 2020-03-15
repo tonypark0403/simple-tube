06ad68af08e60f3b6b191b079bba58d2077123f0
Tony Park <tonypark0403@gmail.com>
2020-03-14 20:31:20 -0400

    added first request to server

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,9 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import axios from "axios";
 function LandingPage() {
+  useEffect(() => {
+    axios.get("/api/user/hello").then(res => console.log(res));
+  }, []);
   return (
     <div>
       <h1>LandingPage</h1>
