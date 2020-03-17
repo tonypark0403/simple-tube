89227d57be5cd9823b136bd7b296f9bc477ca476
Stella Jung <sojung@myseneca.ca>
2020-03-17 08:58:08 -0400

    applied redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import Reducer from "./_reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//createStore is only for object but promise and function types should be accepted.
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, reduxThunk)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}>
+        <App />
+    </Provider>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
