4f8e469d81306a5fc59b3d8f76ca1ff7223a5f11
Stella Jung <sojung@myseneca.ca>
2020-03-17 21:42:17 -0400

    added logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,32 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/user/hello").then(res => console.log(res));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get(`/api/user/logout`)
+      .then(res => {
+        if (res.data.success) {
+          props.history.push("/login");
+        } else {
+          alert('Fail to logout!!!');
+        }
+      });
+  }
+
   return (
     <div style={{
-      display: 'flex', justifyContent: 'center', alignItems: 'center'
-      , width: '100%', height: '100vh'
+      display: 'flex', justifyContent: 'center', alignItems: 'center',
+      flexDirection: 'column', width: '100%', height: '100vh'
     }}>
       <h2>LandingPage</h2>
+      <div>
+        <button onClick={onClickHandler}>
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
